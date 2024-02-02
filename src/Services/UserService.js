b881657d48546e365d7941e4607186cab3906f1b
giovanesalvi@alunos.utfpr.edu.br <giovanelimas16@hotmail.com>
2024-02-02 09:32:05 -0300

    alterando nome service

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -2,21 +2,21 @@ import User from './../Models/User.js';
 
 async function createuser(data, response){
     try {
-        let firstName = data.firstName.trim();
-        let lastName = data.lastName.trim();
-        let password = data.password.trim();
-        let password_2 = data.password_2.trim();
-        let email = data.email.trim();
+        let user = data;
+        user.name = data.name.trim();
+        user.password =  data.password.trim();
+        user.password_2 = data.password_2.trim();
+        user.email = data.email.trim();
 
-        if(firstName === "" || lastName === ""){
-            return response.status(411).json("Insira seu nome e Sobrenome.");
-        } else if(password === "" || password_2 === "" || (password !== password_2)){
-            return response.status(411).json("Senha incorreta.");
-        } else if(!email.includes("@")){
+        if(user.name === ""){
+            return response.status(411).json("Insira seu nome.");
+        } else if(user.password === "" || user.password_2 === "" || (user.password !== user.password_2)){
+            return response.status(411).json("Senha incopatível.");
+        } else if(!user.email.includes("@")){
             return response.status(411).json("Email invalido.");
         }
         
-        await User.create(data);
+        await User.create(user);
         return response.status(201).json("Conta criada com sucesso!");
     } catch (error) {
         return response.status(400).json("Não foi possível criar uma conta. Tente novamente!");
